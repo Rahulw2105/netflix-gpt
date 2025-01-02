930e1561a1625a23caa8d76f39ef209fcda422d5
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-03 01:36:00 +0530

    form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { validateData } from "../utils/validate";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
   function handleSingInForm() {
     setIsSignIn(!isSignIn);
   }
+
+  function handleClickButton() {
+    const message = validateData(email.current.value, password.current.value);
+    console.log(email);
+    console.log(password);
+    console.log(message);
+    setErrorMessage(message);
+  }
+
   return (
     <div>
       <Header />
@@ -16,7 +29,12 @@ const Login = () => {
         />
       </div>
 
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80"
+      >
         <h1 className="text-white text-3xl py-3 ">
           {isSignIn ? "Sign In " : "SignUp"}
         </h1>
@@ -28,16 +46,24 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="email"
           placeholder="email"
           className="p-3 m-2 w-full bg-slate-600"
         />
         <input
+          ref={password}
           type="password"
           placeholder="password"
           className="p-3 m-2 w-full bg-slate-600"
         />
-        <button className="bg-red-700 w-full p-2 m-2 rounded-lg">SignIn</button>
+        <p className="text-red-600"> {errorMessage}</p>
+        <button
+          className="bg-red-700 w-full p-2 m-2 rounded-lg"
+          onClick={handleClickButton}
+        >
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
         <p className="py-3 m-2 cursor:pointer" onClick={handleSingInForm}>
           New to Netflix? Sign Up Now
         </p>
