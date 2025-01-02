cfad8f6087a98d56fc8ce52eb0adc7f39ab59a0b
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-03 01:35:19 +0530

    create form validate function

---
+++ src/utils/validate.js
@@ -0,0 +1,14 @@
+export function validateData(email, password) {
+  const isEmailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      password
+    );
+
+  if (!isEmailValid) return "Email id is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+
+  return null;
+}
