0ca672cd1110625749caff49dc23c3d2703300a6
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-03 16:54:12 +0530

    update title

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
         onSubmit={(e) => {
           e.preventDefault();
         }}
-        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80"
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg"
       >
         <h1 className="text-white text-3xl py-3 ">
           {isSignIn ? "Sign In " : "SignUp"}
