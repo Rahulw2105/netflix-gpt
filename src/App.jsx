0a2925336e1236024951e98c4c363291c74afd22
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-12 02:42:05 +0530

    impllement onAuthStateChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { auth } from "./utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 import Body from "./components/Body";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser, removeUser } from "./utils/userSlice";
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        const { uid, displayName, email } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <>
       <Body />
