092767903e1f803e6f410832cc91c195767c12c4
Rahulw2105 <wankhederahul116@gmail.com>
2025-01-12 02:43:44 +0530

    add sign out button and profile icon and improve styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,40 @@
 import React from "react";
-
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  function handleSignOut() {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
   return (
-    <div className="absolute z-20">
+    <div className="absolute px-8 py-2 z-20 w-screen bg-gradient-to-b from-black flex justify-between">
       <img
         className="w-64 px-8 py-2 bg-gradient-to-b from-black"
         src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      {user && (
+        <div className="flex p-2">
+          <img
+            className="w-12 h-12"
+            src="https://occ-0-3216-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+            alt="logo "
+          />
+          <button className="bg-black h-12 text-white " onClick={handleSignOut}>
+            Sign out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
